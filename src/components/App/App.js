3fe14778ca2adeaf632414aca7424c2c53c521c2
Bartosz Janiak <janiak.bartosz92@gmail.com>
2020-08-15 15:55:21 +0200

    Add new List future and cancel alert confirmation.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
 import styles from './App.scss';
+import Main from "../Main/Main";
+import {pageContents, settings, listMenu, listData} from '../../data/dataStore';
+import Creator from "../Creator/Creator";
 import List from "../List/List";
-import {pageContents, listData} from '../../data/dataStore';
 
 class App extends React.Component {
+    state = {
+        listData: listData.list || [],
+    }
+    addList(title){
+        this.setState(state => (
+            {
+                listData: [
+                    ...state.listData,
+                    {
+                        key: state.listData.length ? state.listData[state.listData.length-1].key+1 : 0,
+                        title,
+                        description: "Interesting things I want to check out!",
+                        image: "http://uploads.kodilla.com/bootcamp/fer/11.react/space.png",
+                        columns: []
+                    }
+                ]
+            }
+        ));
+    }
   render() {
     return (
-      // // <main className={styles.component}>
-      //   <h1 className={styles.title}>My first React app</h1>
-      //   <h2 className={styles.subtitle}>Hello world!</h2>
-      //     <List
-      //         title={['Things to do ', <sup>soon!</sup>]}
-      //         image={'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'}
-      //     >
-      //     </List>
-      // </main>
       <main className={styles.component}>
           <h1 className={styles.title}>{pageContents.title}</h1>
           <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
-          <List {...listData} />
+          <div>
+              <Creator text={settings.listCreatorText} action={title => this.addList(title)}/>
+          </div>
+          <div>
+              {this.state.listData.map(({key, ...listProps}) => (
+                  <List key={key} {...listProps} />
+              ))}
+          </div>
+          <Main {...listMenu} />
       </main>
     )
   }
